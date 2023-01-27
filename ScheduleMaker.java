d4ef02e0c0de57134c1e56bfd81f88bb028bd377
Kathryn Xia <kxia@greenwichacademy.org>
2023-01-26 20:34:35 -0500

    deleted comments, indented

--- ScheduleMaker.java
+++ ScheduleMaker.java
@@ -5,118 +5,92 @@ import java.util.*;
 public class ScheduleMaker {
 
   public static void main(String[] args) throws FileNotFoundException {
-    ArrayList<File> files = new ArrayList<File>(Arrays.asList((new File(args[0])), (new File(args[1]))));
+    ArrayList<File> files = new ArrayList<File>(
+      Arrays.asList((new File(args[0])), (new File(args[1])))
+    );
 
-    ArrayList<APExam> examObjects  = new ArrayList<>();
-    ArrayList<Teacher> teacherObjects  = new ArrayList<>();
+    ArrayList<APExam> examObjects = new ArrayList<>();
+    ArrayList<Teacher> teacherObjects = new ArrayList<>();
 
     generateListOfObjects(files, examObjects, teacherObjects);
 
-    for (APExam exam: examObjects){
+    for (APExam exam : examObjects) {
       System.out.println(exam);
     }
-    for (Teacher teacher: teacherObjects){
+    for (Teacher teacher : teacherObjects) {
       System.out.println(teacher);
     }
 
     System.out.println(((examObjects.get(0))).getName());
-
   }
 
-  public static void generateListOfObjects(List<File> f, ArrayList<APExam> examObjects, ArrayList<Teacher> teacherObjects) throws FileNotFoundException {
-    for (int j = 0; j < f.size(); j ++){
-    
-    Scanner fileScan = new Scanner(f.get(j));
-    fileScan.nextLine();
-    // ArrayList<Object> arrOfInstances  = new ArrayList<Object>();
+  public static void generateListOfObjects(
+    List<File> f,
+    ArrayList<APExam> examObjects,
+    ArrayList<Teacher> teacherObjects
+  ) throws FileNotFoundException {
+    for (int j = 0; j < f.size(); j++) {
+      Scanner fileScan = new Scanner(f.get(j));
+      fileScan.nextLine();
+      // ArrayList<Object> arrOfInstances  = new ArrayList<Object>();
 
-    while (fileScan.hasNextLine()) {
-      String line = fileScan.nextLine();
+      while (fileScan.hasNextLine()) {
+        String line = fileScan.nextLine();
 
-      ArrayList<ArrayList<String>> instanceVarValues = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> instanceVarValues = new ArrayList<ArrayList<String>>();
 
-      for (int i = 0; i < line.length() - 1; i++) {
-        if (line.charAt(i) == '"') {
-          int nextQuote = line.indexOf("\"", i + 1);
-          instanceVarValues.add(
-            new ArrayList<String>(
-              Arrays.asList(line.substring(i + 1, nextQuote).split(","))
-            )
-          );
-          i = nextQuote + 1;//"moving" i to the end of the quote
-        } 
-        
-        else {
-          int nextComma = line.indexOf(",", i + 1);
-
-          if (nextComma == -1) {
-            nextComma = line.length();
-          }
-
-          instanceVarValues.add(
-            new ArrayList<String>(
-              Arrays.asList(
-                line.substring(i, nextComma)
+        for (int i = 0; i < line.length() - 1; i++) {
+          if (line.charAt(i) == '"') {
+            int nextQuote = line.indexOf("\"", i + 1);
+            instanceVarValues.add(
+              new ArrayList<String>(
+                Arrays.asList(line.substring(i + 1, nextQuote).split(","))
               )
-            )
-          );
-          i = nextComma;
+            );
+            i = nextQuote + 1; //"moving" i to the end of the quote
+          } 
+          else {
+            int nextComma = line.indexOf(",", i + 1);
+
+            if (nextComma == -1) {
+              nextComma = line.length();
+            }
+
+            instanceVarValues.add(
+              new ArrayList<String>(Arrays.asList(line.substring(i, nextComma))));
+            i = nextComma;
+          }
+        }
+
+        if (((f.get(j)).getName()).contains("eacher")) {
+          teacherObjects.add(new Teacher(instanceVarValues));
+        } 
+        else {
+          examObjects.add(new APExam(instanceVarValues));
         }
       }
-
-      if (((f.get(j)).getName()).contains("eacher")){
-        teacherObjects.add(new Teacher(instanceVarValues)); 
-      }
-
-      else {
-        examObjects.add(new APExam(instanceVarValues));
-      }
-    }
     }
   }
-
-  // public static void downcast(ArrayList<T> arr, String s){//because the generateListOfObjects returns an object array, this method downcasts all the instances so we can access the methods of the APExam an Teacher classes.
-  //   for (int i = 0; i < arr.size(); i++){
-
-  //     if (s.equals("exam")){
-  //       arr.set(i, ((APExam)(arr.get(i))));
-  //       ((APExam)(arr.get(i))).getName();
-  //     }
-
-  //     else if (s.equals("teacher")){
-  //        arr.set(i, ((Teacher)(arr.get(i))));
-  //        ((Teacher)(arr.get(i))).getName();
-
-  //     }
-
-  //   }
-  // }
-
+  
   public static String testerTimeConverter(String block, int date) {
     String timeToRet = "";
     //pretending there are only 2 possible dates and hard coding what time each block would be
     if (date == 27) {
       if (block.equals("A")) {
         timeToRet += "8:00-9:00am";
-      }
-      else if (block.equals("B")) {
+      } else if (block.equals("B")) {
         timeToRet += "9:00-10:00am";
-      }
-      else if (block.equals("C")) {
+      } else if (block.equals("C")) {
         timeToRet += "10:00-11:00am";
       }
-    } 
-    else {
+    } else {
       if (block.equals("D")) {
         timeToRet += "11:00-12:00pm";
-      } 
-      else if (block.equals("E")) {
+      } else if (block.equals("E")) {
         timeToRet += "12:00-1:00pm";
-      } 
-      else if (block.equals("F")) {
+      } else if (block.equals("F")) {
         timeToRet += "1:00-2:00pm";
-      } 
-      else {
+      } else {
         timeToRet += "2:00-3:00pm";
       }
     }
