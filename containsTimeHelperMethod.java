943e4753f36b0a28e697ac3ee13af0ab419a9131
Your Name <cassidyrosemckee@gmail.com>
2023-01-30 08:43:35 -0500

    added helper method formatTime and implemented in containsTime elper method

--- containsTimeHelperMethod.java
+++ containsTimeHelperMethod.java
@@ -2,13 +2,19 @@ import java.time.*;
 
 public class containsTimeHelperMethod{
     public static void main(String[] args){
-        String outerStart = "07:59";
+        String outerStart = "7:59";
         String outerEnd = "11:01";
         String innerStart = "08:00";
         String innerEnd = "11:00";
-        System.out.println(containsTime(outerStart, outerEnd, innerStart, innerEnd));
+        //System.out.println(containsTime(outerStart, outerEnd, innerStart, innerEnd));
+        //System.out.println(formatTime(outerStart));
+        //System.out.println(formatTime(outerEnd));
     }
     public static boolean containsTime(String outerTimeStart, String outerTimeEnd, String innerTimeStart, String innerTimeEnd){
+        formatTime(outerTimeStart);
+        formatTime(outerTimeEnd);
+        formatTime(innerTimeStart);
+        formatTime(innerTimeEnd);
         LocalTime outerStart = LocalTime.parse(outerTimeStart);
         LocalTime outerEnd = LocalTime.parse(outerTimeEnd); 
         LocalTime innerStart = LocalTime.parse(innerTimeStart); 
@@ -19,6 +25,16 @@ public class containsTimeHelperMethod{
         }
         else{
             return false;
+        }
+    }
+
+    public static String formatTime(String time){
+        if (time.length()==5){
+            return time;
+        }
+        else{
+            String newTime = "0" + time;
+            return newTime;
         }
     }
 
