22f507d5fa9ae70d4350b424437385070e80acc9
Your Name <cassidyrosemckee@gmail.com>
2023-01-30 08:37:22 -0500

    no errors in compiling or run time because of adjusted time format but still need to create a time formatter

--- containsTimeHelperMethod.java
+++ containsTimeHelperMethod.java
@@ -1,20 +1,20 @@
-import java.time.Localtime;
+import java.time.*;
 
 public class containsTimeHelperMethod{
     public static void main(String[] args){
-        String outerStart = "7:59";
+        String outerStart = "07:59";
         String outerEnd = "11:01";
-        String innerStart = "8:00";
+        String innerStart = "08:00";
         String innerEnd = "11:00";
         System.out.println(containsTime(outerStart, outerEnd, innerStart, innerEnd));
     }
     public static boolean containsTime(String outerTimeStart, String outerTimeEnd, String innerTimeStart, String innerTimeEnd){
-        LocalTime outerStart = Localtime.parse(outerTimeStart);
-        LocalTime outerEnd = Localtime.parse(outerTimeEnd); 
-        LocalTime innerStart = Localtime.parse(innerTimeStart); 
-        LocalTime innerEnd = Localtime.parse(innerTimeEnd);
+        LocalTime outerStart = LocalTime.parse(outerTimeStart);
+        LocalTime outerEnd = LocalTime.parse(outerTimeEnd); 
+        LocalTime innerStart = LocalTime.parse(innerTimeStart); 
+        LocalTime innerEnd = LocalTime.parse(innerTimeEnd);
 
-        if (outerTimeStart.isBefore(innerTimeStart) && outerTimeEnd.isAfter(innerTimeEnd)){
+        if (outerStart.isBefore(innerStart) && outerEnd.isAfter(innerEnd)){
             return true;
         }
         else{
