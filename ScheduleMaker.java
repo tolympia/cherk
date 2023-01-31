706058725b21d97895312e984a2344cf204186c0
Kathryn Xia <kxia@greenwichacademy.org>
2023-01-31 11:32:59 -0500

    fixing string issues

--- ScheduleMaker.java
+++ ScheduleMaker.java
@@ -167,18 +167,18 @@ public class ScheduleMaker {
     }
   }
 
-  public static void writeIn(Map<<String>, <List<String>>> proctorMap, List<APExam> examList){
+  public static void writeIn(Map<String, List<String>> proctorMap, List<APExam> examList){
     //create new file with printsteram
     PrintStream p = new PrintStream("ApExamProctorSchedule.csv");
     //print headers into the csv 
-    p.println("AP Exam", "Exam Date", "Exam Start Time", "Exam End Time", "Proctors");
+    p.println("AP Exam" + "Exam Date" + "Exam Start Time" + "Exam End Time" + "Proctors");
     //loop through map to print to csv
     for(int i=0; i<examList.size(); i++){
       APExam exam = examList.get(i);
       //how do i use .getDate() and .getTime() for specific AP exams
       List<String> proctorNames = proctorMap.get(exam.getName());
       String proctors = arrayFormat(proctorNames);
-      p.println(exam.getName(), exam.getDate(), exam.getStartTime(), exam.getEndTime(), proctors);
+      p.println(exam.getName() + exam.getDate() + exam.getStartTime() + exam.getEndTime() + proctors);
     }
     p.close();
   }
