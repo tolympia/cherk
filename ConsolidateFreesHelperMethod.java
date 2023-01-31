af9f63d3e39a3745d50f478ece2dcc09195faa28
Kathryn Xia <kxia@greenwichacademy.org>
2023-01-31 11:56:06 -0500

    fixing some size errors

--- ConsolidateFreesHelperMethod.java
+++ ConsolidateFreesHelperMethod.java
@@ -22,46 +22,47 @@ public class ConsolidateFreesHelperMethod{
     }
 
     public static ArrayList<ArrayList<String>> consolidateFrees(Teacher teacher, String date){
-        // List<String> frees = teacher.getFreePeriods();
-        // List<String> freesOnDay = date.getFrees(); //this will be using Mrs. Zhu's code 
+        List<String> frees = teacher.getFreePeriods();
+        List<String> freesOnDay = date.getFrees(); //this will be using Mrs. Zhu's code 
         
         
-        // for (int i=0; i<frees.length; i++){
-        //     if (!freesOnDay.contains(frees.get(i))){
-        //         frees.remove(frees.get(i));
-        //     }
-        // }
+        for (int i=0; i<frees.size(); i++){
+            if (!freesOnDay.contains(frees.get(i))){
+                frees.remove(frees.get(i));
+            }
+        }
 
-        // ArrayList<ArrayList<LocalTime>> freesTimes = new ArrayList<ArrayList<LocalTime>>();
-        // for (int i=0; i<frees.length; i++){
-        //     ArrayList<LocalTime> startAndEnd = new ArrayList<>();
+        ArrayList<ArrayList<LocalTime>> freesTimes = new ArrayList<ArrayList<LocalTime>>();
+        for (int i=0; i<frees.length; i++){
+            ArrayList<LocalTime> startAndEnd = new ArrayList<>();
             
-        //     LocalTime start = new LocalTime(getStart(frees.get(i)));//using mrs zhu's code to turn block into start time
-        //     LocalTime end = new LocalTime(getEnd(frees.get(i)));//using mrs zhu's code to turn block into end time
-        //     startAndEnd.add(start);
-        //     startAndEnd.add(end); 
-        //     freesTimes.add(startAndEnd);
-        // }
+            LocalTime start = new LocalTime(getStart(frees.get(i)));//using mrs zhu's code to turn block into start time
+            LocalTime end = new LocalTime(getEnd(frees.get(i)));//using mrs zhu's code to turn block into end time
+            startAndEnd.add(start);
+            startAndEnd.add(end); 
+            freesTimes.add(startAndEnd);
+        }
+    
 
-        // ArrayList<ArrayList<LocalTime>> timesInOrder = sort(freeTimes);//using robyn's sort elper methodsw
+        ArrayList<ArrayList<LocalTime>> timesInOrder = sort(freeTimes);//using robyn's sort elper methodsw
         ArrayList<ArrayList<String>> consolidatedFrees = new ArrayList<ArrayList<String>>();
-        // for (int i=1; i<timesInOrder.length()-1; i++){
-        //     ArrayList<LocalTime> timeFrame1 = timesInOrder.get(i-1);
-        //     ArrayList<LocalTime> timeFrame2 = timesInOrder.get(i);
-        //     LocalTime time1End = timeFrame1.get(1);
-        //     LocalTime time2Start = timeFrame2.get(0);
+        for (int i=1; i<timesInOrder.size()-1; i++){
+            ArrayList<LocalTime> timeFrame1 = timesInOrder.get(i-1);
+            ArrayList<LocalTime> timeFrame2 = timesInOrder.get(i);
+            LocalTime time1End = timeFrame1.get(1);
+            LocalTime time2Start = timeFrame2.get(0);
 
-        //     ArrayList<LocalTime> newTimeBlock = new ArrayList<>();
-        //     if(time2Start.minusMinutes(11).isBefore(time1End)){
-        //         newTimeBlock.add(timeFrame1.get(0));
-        //         newTimeBlock.add(timeFrame2.get(1));
-        //     }
-        //     else{
-        //         newTimeBlock.add(timeFrame1.get(0));
-        //         newTimeBlock.add(time1End);
-        //     }
-        //     consolidatedFrees.add(newTimeBlock);
-        // }
+            ArrayList<LocalTime> newTimeBlock = new ArrayList<>();
+            if(time2Start.minusMinutes(11).isBefore(time1End)){
+                newTimeBlock.add(timeFrame1.get(0));
+                newTimeBlock.add(timeFrame2.get(1));
+            }
+            else{
+                newTimeBlock.add(timeFrame1.get(0));
+                newTimeBlock.add(time1End);
+            }
+            consolidatedFrees.add(newTimeBlock);
+        }
 
         return consolidatedFrees;
 
