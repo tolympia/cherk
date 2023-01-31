662866b71da23ab089a05d179fe84a351484cd54
Your Name <cassidyrosemckee@gmail.com>
2023-01-31 12:59:27 -0500

    Changed format and testing sort method now

--- ConsolidateFreesHelperMethod.java
+++ ConsolidateFreesHelperMethod.java
@@ -21,67 +21,66 @@ public class ConsolidateFreesHelperMethod{
         sort(freesTimes); 
     }
 
-    public static ArrayList<ArrayList<String>> consolidateFrees(Teacher teacher, String date){
-        List<String> frees = teacher.getFreePeriods(); //WHERE ID GETFREEPERIODS LOCATED
-        List <String> freesOnDay = new List<String>;
-        if (getTimeFromBlockAndDate("A", date)!=null){
-            freesOnDay.add("A");
-        }
-        if (getTimeFromBlockAndDate("B", date)!=null){
-            freesOnDay.add("B");
-        }
-        if (getTimeFromBlockAndDate("C", date)!=null){
-            freesOnDay.add("C");
-        }
-        if (getTimeFromBlockAndDate("D", date)!=null){
-            freesOnDay.add("D");
-        }
-        if (getTimeFromBlockAndDate("E", date)!=null){
-            freesOnDay.add("E");
-        }
-        if (getTimeFromBlockAndDate("F", date)!=null){
-            freesOnDay.add("F");
-        }
-        if (getTimeFromBlockAndDate("G", date)!=null){
-            freesOnDay.add("G");
-        } 
+    // public static ArrayList<ArrayList<String>> consolidateFrees(Teacher teacher, String date){
+    //     List<String> frees = teacher.getFreePeriods(); //WHERE IS GETFREEPERIODS LOCATED
+    //     List <String> freesOnDay = new List<String>;
+    //     if (getTimeFromBlockAndDate("A", date)!=null){
+    //         freesOnDay.add("A");
+    //     }
+    //     if (getTimeFromBlockAndDate("B", date)!=null){
+    //         freesOnDay.add("B");
+    //     }
+    //     if (getTimeFromBlockAndDate("C", date)!=null){
+    //         freesOnDay.add("C");
+    //     }
+    //     if (getTimeFromBlockAndDate("D", date)!=null){
+    //         freesOnDay.add("D");
+    //     }
+    //     if (getTimeFromBlockAndDate("E", date)!=null){
+    //         freesOnDay.add("E");
+    //     }
+    //     if (getTimeFromBlockAndDate("F", date)!=null){
+    //         freesOnDay.add("F");
+    //     }
+    //     if (getTimeFromBlockAndDate("G", date)!=null){
+    //         freesOnDay.add("G");
+    //     } 
         
         
-        for (int i=0; i<frees.size(); i++){
-            if (!freesOnDay.contains(frees.get(i))){
-                frees.remove(frees.get(i));
-            }
-        }
+    //     for (int i=0; i<frees.size(); i++){
+    //         if (!freesOnDay.contains(frees.get(i))){
+    //             frees.remove(frees.get(i));
+    //         }
+    //     }
 
-        ArrayList<ArrayList<LocalTime>> freesTimes = new ArrayList<ArrayList<LocalTime>>();
-        for (int i=0; i<frees.length; i++){
-            ArrayList<LocalTime> startAndEnd = getTimeFromBlockAndDate(frees.get(i), date);
-            freesTimes.add(startAndEnd);
-        }
+    //     ArrayList<ArrayList<LocalTime>> freesTimes = new ArrayList<ArrayList<LocalTime>>();
+    //     for (int i=0; i<frees.length; i++){
+    //         ArrayList<LocalTime> startAndEnd = getTimeFromBlockAndDate(frees.get(i), date);
+    //         freesTimes.add(startAndEnd);
+    //     }
     
-        ArrayList<ArrayList<LocalTime>> timesInOrder = sort(freeTimes);//using robyn's sort elper methodsw
+    //     ArrayList<ArrayList<LocalTime>> timesInOrder = sort(freeTimes);//using robyn's sort elper methodsw
         
-        ArrayList<ArrayList<String>> consolidatedFrees = new ArrayList<ArrayList<String>>();
-        for (int i=1; i<timesInOrder.size()-1; i++){
-            ArrayList<LocalTime> timeFrame1 = timesInOrder.get(i-1);
-            ArrayList<LocalTime> timeFrame2 = timesInOrder.get(i);
-            LocalTime time1End = timeFrame1.get(1);
-            LocalTime time2Start = timeFrame2.get(0);
+    //     ArrayList<ArrayList<String>> consolidatedFrees = new ArrayList<ArrayList<String>>();
+    //     for (int i=1; i<timesInOrder.size()-1; i++){
+    //         ArrayList<LocalTime> timeFrame1 = timesInOrder.get(i-1);
+    //         ArrayList<LocalTime> timeFrame2 = timesInOrder.get(i);
+    //         LocalTime time1End = timeFrame1.get(1);
+    //         LocalTime time2Start = timeFrame2.get(0);
 
-            ArrayList<LocalTime> newTimeBlock = new ArrayList<>();
-            if(time2Start.minusMinutes(11).isBefore(time1End)){
-                newTimeBlock.add(timeFrame1.get(0));
-                newTimeBlock.add(timeFrame2.get(1));
-            }
-            else{
-                newTimeBlock.add(timeFrame1.get(0));
-                newTimeBlock.add(time1End);
-            }
-            consolidatedFrees.add(newTimeBlock);
-        }
-        return consolidatedFrees;
-
-    }
+    //         ArrayList<LocalTime> newTimeBlock = new ArrayList<>();
+    //         if(time2Start.minusMinutes(11).isBefore(time1End)){
+    //             newTimeBlock.add(timeFrame1.get(0));
+    //             newTimeBlock.add(timeFrame2.get(1));
+    //         }
+    //         else{
+    //             newTimeBlock.add(timeFrame1.get(0));
+    //             newTimeBlock.add(time1End);
+    //         }
+    //         consolidatedFrees.add(newTimeBlock);
+    //     }
+    //     return consolidatedFrees;
+    // }
 
     public static ArrayList<ArrayList<LocalTime>> sort(ArrayList<ArrayList<LocalTime>> freesTimes){
         ArrayList<ArrayList<LocalTime>> sortedTimes = new ArrayList<ArrayList<LocalTime>>(); 
@@ -118,51 +117,51 @@ public class ConsolidateFreesHelperMethod{
     }
 
 //*****MRS ZHUS CODE */
-    public static ArrayList<LocalTime> getTimeFromBlockAndDate(String blockName, String date) {
-        // Convert date to a Date object.
-        LocalDate dateObject = LocalDate.parse(date);
-        // Get schedule for this date.
-        USSchedule schedule = getUSScheduleForDate(dateObject);
-        // Get list of block names.
-        ArrayList<String> blockNames = schedule.blocksForDayType();
+    // public static ArrayList<LocalTime> getTimeFromBlockAndDate(String blockName, String date) {
+    //     // Convert date to a Date object.
+    //     LocalDate dateObject = LocalDate.parse(date);
+    //     // Get schedule for this date.
+    //     USSchedule schedule = getUSScheduleForDate(dateObject);
+    //     // Get list of block names.
+    //     ArrayList<String> blockNames = schedule.blocksForDayType();
 
-        // Find the index of the desired block within all the blocks.
-        int blockIndex = blockNames.indexOf(blockName);
-        if (blockIndex == -1) {
-            // This block (e.g. A) does not occur on this date, so return null.
-            return null;
-        }
-        // Get the corresponding Block object, which contains start and end times.
-        Block thisBlock = schedule.blocks.get(blockIndex);
-        LocalTime startTime = thisBlock.startTime;
-        LocalTime endTime = thisBlock.endTime;
-        return new ArrayList<LocalTime>(Arrays.asList(startTime, endTime));
-    }
+    //     // Find the index of the desired block within all the blocks.
+    //     int blockIndex = blockNames.indexOf(blockName);
+    //     if (blockIndex == -1) {
+    //         // This block (e.g. A) does not occur on this date, so return null.
+    //         return null;
+    //     }
+    //     // Get the corresponding Block object, which contains start and end times.
+    //     Block thisBlock = schedule.blocks.get(blockIndex);
+    //     LocalTime startTime = thisBlock.startTime;
+    //     LocalTime endTime = thisBlock.endTime;
+    //     return new ArrayList<LocalTime>(Arrays.asList(startTime, endTime));
+    // }
 
-    private static USSchedule getUSScheduleForDate(LocalDate date) {
-        DayOfWeek day = date.getDayOfWeek();
+    // private static USSchedule getUSScheduleForDate(LocalDate date) {
+    //     DayOfWeek day = date.getDayOfWeek();
 
-        USSchedule schedule;
+    //     USSchedule schedule;
 
-        // Determine if this is an adjusted schedule day.
-        if (USSchedule.adjustedSchedules.containsKey(date)) {
-            String adjustedType = USSchedule.adjustedSchedules.get(date);
-            schedule = new USAdjusted1(date, true);
-        } else {
-            boolean flexDay = day == DayOfWeek.MONDAY || day == DayOfWeek.TUESDAY || day == DayOfWeek.THURSDAY;
-            if (flexDay) {
-                schedule = new USFlexDay(date, true);
-            } else if (day == DayOfWeek.FRIDAY) {
-                schedule = new USFriday(date, true);
-            } else {  // Wed schedule
-                schedule = new USWednesday(date, true);
-            }
-            if (schedule.getDayType() == -1) {
-                // Not a real school day.
-                return null;
-            }
-        }
-        return schedule;
-    }
+    //     // Determine if this is an adjusted schedule day.
+    //     if (USSchedule.adjustedSchedules.containsKey(date)) {
+    //         String adjustedType = USSchedule.adjustedSchedules.get(date);
+    //         schedule = new USAdjusted1(date, true);
+    //     } else {
+    //         boolean flexDay = day == DayOfWeek.MONDAY || day == DayOfWeek.TUESDAY || day == DayOfWeek.THURSDAY;
+    //         if (flexDay) {
+    //             schedule = new USFlexDay(date, true);
+    //         } else if (day == DayOfWeek.FRIDAY) {
+    //             schedule = new USFriday(date, true);
+    //         } else {  // Wed schedule
+    //             schedule = new USWednesday(date, true);
+    //         }
+    //         if (schedule.getDayType() == -1) {
+    //             // Not a real school day.
+    //             return null;
+    //         }
+    //     }
+    //     return schedule;
+    // }
 
 }
